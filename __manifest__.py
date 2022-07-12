814de49a24c3984e0da9ba0bb0625c5a8a6ce9b0
Cesar250101 <cesar250101@outlook.com>
2022-07-12 17:22:55 -0400

    Corta texto en comentarios

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','account','stock'],
+    'depends': ['base','sale','account','stock','sale_stock'],
 
     # always loaded
     'data': [

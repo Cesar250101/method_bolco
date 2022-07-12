7f00007e48d22f9bb556d5602f5e7b9e109251b2
Cesar250101 <cesar250101@outlook.com>
2022-07-12 17:10:44 -0400

    Corta texto en comentarios

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from cgitb import text
 from odoo import models, fields, api
 
 
@@ -14,8 +15,23 @@ class Factura(models.Model):
     _inherit = 'stock.picking'
 
     notas_despacho = fields.Text(string='Nota Despacho',related='sale_id.notas_despacho')
-    
 
+class Facturas(models.Model):
+    _inherit = 'account.invoice'
 
     
+    @api.model
+    def create(self, values):
+        texto=values['comment']
+        texto_cortado=''
+        inicio=0
+        fin=60
+        ciclos=int(len(texto)/fin)
+        if len(texto)>fin:
+            for t in range(ciclos):
+                texto_cortado+=texto[inicio:fin]+'\n'
+                inicio=fin
+                fin+=60
+        values['comment']=texto_cortado
+        return super(Facturas, self).create(values)
     

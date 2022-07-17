ccd0804243bef7bb6ae6d9e847bab9ebbf66b6d0
Cesar250101 <cesar250101@outlook.com>
2022-07-17 18:59:36 -0400

    Conciliacion

--- models/models.py
+++ models/models.py
@@ -35,4 +35,20 @@ class Factura(models.Model):
                 fin+=60
         values['comment']=texto_cortado
         return super(Factura, self).create(values)
+
+
+class Conciliacion(models.Model):
+    _inherit = 'account.bank.statement.line'
     
+
+    def process_reconciliation(self, counterpart_aml_dicts=None, payment_aml_rec=None, new_aml_dicts=None):
+        conciliacion=super(Conciliacion, self).process_reconciliation()        
+        payment_aml_rec.write({'statement_line_id': self.id})
+
+        # arma la sentencia SQL
+        qry = "UPDATE account_move_line SET statement_line_id = {} WHERE id ={}"
+
+        qry=qry.format(self.id,payment_aml_rec.id)
+        self.env.cr.execute(qry)        
+        return payment_aml_rec
+
